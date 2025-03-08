743458dbb118d909b52aacde7b28be12bd7825c5
丘惟夫 <waveciou@gmail.com>
2025-03-08 12:16:50 +0800

    local search settings

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -202,6 +202,23 @@ const config = {
         darkTheme: prismThemes.dracula,
       },
     }),
+
+    themes: [
+      [
+        '@easyops-cn/docusaurus-search-local',
+        {
+          // ... Your options.
+          // `hashed` is recommended as long-term-cache of index file is possible.
+          hashed: true,
+  
+          // For Docs using Chinese, it is recomended to set:
+          // language: ["en", "zh"],
+  
+          // If you're using `noIndex: true`, set `forceIgnoreNoIndex` to enable local index:
+          // forceIgnoreNoIndex: true,
+        }
+      ],
+    ],
 };
 
 export default config;
